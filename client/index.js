ae8ca12e87cf8dc5cdca19f28374a4cda2521334
Carl Sverre <accounts@carlsverre.com>
2016-11-11 12:27:45 -0800

    channels

--- client/index.js
+++ client/index.js
@@ -3,11 +3,12 @@ import "./adapter";
 import iceServers from "./iceservers";
 
 var remoteView, localView, localStream;
-var mode = "unknown";
+var channelNameEl;
 
 var start = () => {
     remoteView = document.getElementById("remote");
     localView = document.getElementById("local");
+    channelNameEl = document.getElementById("channel");
 
     navigator.getUserMedia({ audio: true, video: true }, stream => {
         localStream = stream;
@@ -20,6 +21,17 @@ var startRTC = () => {
     var wsProto = window.location.protocol === "https:" ? "wss" : "ws";
     var ws = new WebSocket(`${wsProto}://${location.host}/connect`);
     var peerConnection;
+
+    Array.prototype.forEach.call(document.getElementsByClassName("channel-btn"), btn => {
+        btn.addEventListener("click", () => {
+            console.log(`joining channel ${btn.dataset.channel}`);
+            channelNameEl.innerHTML = btn.dataset.channel;
+            resetPeerConnection();
+            ws.send(JSON.stringify({
+                channel: btn.dataset.channel
+            }));
+        });
+    });
 
     var resetPeerConnection = () => {
         if (peerConnection) {
@@ -50,21 +62,17 @@ var startRTC = () => {
     };
 
     ws.onopen = () => {
-        resetPeerConnection();
-
         ws.onmessage = evt => {
             var signal = JSON.parse(evt.data);
 
-            if (signal.mode) {
+            if (signal.error) {
+                console.error(signal.error);
+            } else if (signal.mode) {
                 console.log(`I am ${signal.mode}`);
+                resetPeerConnection();
                 if (signal.mode === "master") {
-                    if (mode === "slave") {
-                        // we are switching from slave -> master
-                        resetPeerConnection();
-                    }
                     dial();
                 }
-                mode = signal.mode;
             } else if (signal.candidate) {
                 console.log("received candidate");
                 peerConnection.addIceCandidate(
--- index.js
+++ index.js
@@ -6,32 +6,90 @@ require("express-ws")(app);
 app.use("/assets", express.static("assets"));
 app.use("/", express.static("views"));
 
-const connections = [];
+function Channel(name) {
+    this.conns = [];
+    this.name = name;
 
-function updateMaster() {
-    if (connections.length > 0) {
-        connections[0].send(JSON.stringify({ mode: "master" }));
-        for (var i = 1; i < connections.length; i++) {
-            connections[i].send(JSON.stringify({ mode: "slave" }));
+    this.addConnection = (ws) => {
+        if (this.conns.length === 2) {
+            throw new Error("Channel is at capacity");
         }
-    }
+        this.conns.push(ws);
+        this.broadcastRoles();
+    };
+
+    this.removeConnection = (ws) => {
+        var idx = this.conns.indexOf(ws);
+        if (idx >= 0) { this.conns.splice(idx, 1); }
+    };
+
+    this.broadcastRoles = () => {
+        for (var i = 0; i < this.conns.length; i++) {
+            this.conns[i].send(JSON.stringify({
+                mode: i === 0 ? "master" : "slave"
+            }));
+        }
+    };
+
+    this.broadcast = (source, msg) => {
+        this.conns.forEach(conn => {
+            if (conn !== source) { conn.send(msg); }
+        });
+    };
+
+    this.empty = () => {
+        return this.conns.length === 0;
+    };
 }
 
+const CHANNELS = [];
+
+var findChannel = name => {
+    for (var i = 0; i < CHANNELS.length; i++) {
+        if (CHANNELS[i].name === name) { return CHANNELS[i]; }
+    }
+};
+
+var removeConnFromChannel = (channel, ws) => {
+    channel.removeConnection(ws);
+    if (channel.empty()) {
+        var idx = CHANNELS.indexOf(channel);
+        if (idx >= 0) { CHANNELS.splice(idx, 1); }
+    }
+};
+
 app.ws("/connect", function(ws) {
-    connections.push(ws);
-    updateMaster();
+    var channel;
 
     ws.on("message", function(msg) {
-        connections.forEach(c => {
-            if (c !== ws) { c.send(msg); }
-        });
+        try {
+            var data = JSON.parse(msg);
+            if (data.channel) {
+                if (channel) {
+                    removeConnFromChannel(channel, ws);
+                }
+
+                channel = findChannel(data.channel);
+                if (!channel) {
+                    channel = new Channel(data.channel);
+                    CHANNELS.push(channel);
+                }
+                channel.addConnection(ws);
+            } else {
+                if (channel) {
+                    channel.broadcast(ws, msg);
+                } else {
+                    throw new Error("No channel selected");
+                }
+            }
+        } catch(err) {
+            ws.send(JSON.stringify({ error: err }));
+        }
     });
 
     ws.on("close", function() {
-        var idx = connections.indexOf(ws);
-        if (idx >= 0) {
-            connections.splice(idx, 1);
-            updateMaster();
+        if (channel) {
+            removeConnFromChannel(channel, ws);
         }
     });
 });
